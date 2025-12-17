4057bc3ed417ad703e0864eeb2e46a2dd99dba1e
Dean <dean@thecodeengine.co.uk>
2025-12-17 10:09:13 +0000

    fixed optional param error.

--- src/Components/Params/BodyParameter.php
+++ src/Components/Params/BodyParameter.php
@@ -6,14 +6,22 @@ use SwaggerBuilder\Components\Schema;
 
 class BodyParameter extends BaseParameter
 {
-    public function __construct($name = 'id', bool $required = false, Schema $schema)
+    /**
+     * @param string $name
+     * @param bool   $required
+     * @param Schema|null $schema
+     */
+    public function __construct($name = 'id', bool $required = false, ?Schema $schema = null)
     {
         parent::__construct($name, static::BODY, static::BODY, $required);
         unset($this->structure['type']);
-        $this->setSchema($schema);
+
+        if ($schema !== null) {
+            $this->setSchema($schema);
+        }
     }
 
-    private function setSchema(Schema $schema): BodyParameter
+    private function setSchema(?Schema $schema): BodyParameter
     {
         return $this->set('schema', $schema);
     }
